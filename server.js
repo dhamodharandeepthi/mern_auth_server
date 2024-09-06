b229308a5bb764b251610e517f8f30f49b5007ba
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2024-09-06 12:23:46 +0530

    initial route test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const databaseConnection = require('./config/db');
-const express = require('express');
-const cors = require('cors');
+const databaseConnection = require("./config/db");
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 databaseConnection();
 const PORT = process.env.PORT || 8000;
@@ -10,7 +10,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+//route
+app.get("/", (req, res) => {
+  res.send("welcome");
+});
+
 
 app.listen(PORT, () => {
-    console.log("server running..:::",`http://localhost:${PORT}`)
-})
+  console.log("server running..:::", `http://localhost:${PORT}`);
+});
