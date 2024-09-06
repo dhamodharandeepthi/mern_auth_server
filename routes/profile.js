1c2325b4f97ad8afe46ce8befe1209d87b7d4a76
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2024-09-06 16:53:13 +0530

    get profile data

--- routes/profile.js
+++ routes/profile.js
@@ -5,21 +5,25 @@ const User = require("../models/User");
 const router = express.Router();
 
 const authenticateToken = (req, res, next) => {
-    
-    const token = req.headers["authorization"]?.split(" ")[1];
-    if (token) {
-        jwt.verify(token, process.env.JWT_KEY, (err, user) => {
-            if (err) return sendStatus(403);
-            req.user = user;
-            next();
-        })
-    } else {
-        res.sendStatus(401);
-    }
-    
-
+  const token = req.headers["authorization"]?.split(" ")[1];
+  if (token) {
+    jwt.verify(token, process.env.JWT_KEY, (err, user) => {
+      if (err) return sendStatus(403);
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
 };
 
-router.get("/", authenticateToken);
+router.get("/", authenticateToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: "fetching profile error" });
+  }
+});
 
 module.exports = router;
